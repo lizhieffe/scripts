b6ca5e38f3b1745334a1949b14951d64c2ac69a2
Zhi Li <lizhieffe@gmail.com>
2015-03-28 09:43:15 -0400

    add functionality

--- android/print_activity_stack.sh
+++ android/print_activity_stack.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/bash
 
-adb shell dumpsys activity | grep -i run
+if [ $# -gt 1 ]; then
+    echo "Rationale : List the activity stack for all tasks of the given package."
+    echo "Usage     : $(basename $0)"
+    echo "Usage     : $(basename $0) <package>"
+    exit 1
+fi
+
+if [ $# -eq 0 ]; then
+	adb shell dumpsys activity | grep -i run
+else
+	adb shell dumpsys activity | grep -i run | grep $1
+fi
